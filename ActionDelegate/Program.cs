4ef425b20c919ac983c2c8c0143934a8d9b62c54
Darius <dariuszdroba@gmail.com>
2024-03-16 14:02:04 +0200

    Action Delegate

---
+++ ActionDelegate/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ActionDelegate
+{
+    internal class Program
+    {
+        public delegate void Print(int val);
+        static void ConsolePrint(int i)
+        {
+            Console.WriteLine(i);
+        }
+        static void Main(string[] args)
+        {
+            // Action Delegate similar with Func delegate but no return value (void)
+            // regular delegate example
+            Print prnt = ConsolePrint;
+            prnt(10);
+
+            // Action alternative
+            Action<int> printActionDel = ConsolePrint;
+            printActionDel(10);
+
+            // Can also be defined with instantiating by passing function to delegate in constructor
+            // Action<int> printAction = new Action<int>(ConsolePrint); 
+
+            // Anonymous method assigned
+            Action helloWorld = delegate ()
+            {
+                Console.WriteLine("Hello World");
+            };
+            helloWorld();
+            // Lambda expression method action
+            Action lambdaHello = () => Console.WriteLine("Hello from lambda method action");
+            lambdaHello();
+        }
+    }
+}
