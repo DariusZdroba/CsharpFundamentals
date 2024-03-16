55fdabada82d3b5d529a34f182949e85cc0290d1
Darius <dariuszdroba@gmail.com>
2024-03-16 14:23:45 +0200

    Predicate Delegates

---
+++ PredicateDelegate/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PredicateDelegate
+{
+    internal class Program
+    {
+        static bool isUpperCase(string s)
+        {
+            return s.Equals(s.ToUpper());
+        }
+        static void Main(string[] args)
+        {
+            // Predicate delegates similar to Action and Func, returns boolean type, validating an input of specified type
+            Predicate<string> isUpper = isUpperCase;
+            Console.WriteLine(isUpper("hello WORLD")); // will output false
+
+            // Anonymous and lambda alternative:
+            Predicate<string> isLower = delegate (string s) { return s.Equals(s.ToLower()); };
+            Console.WriteLine(isLower("hello world")); // will output true
+
+            Predicate<string> endsWithA = (string s) => { return s.ToLower().EndsWith("a"); };
+            Console.WriteLine(endsWithA("holla"));  // will output true
+        }
+    }
+}
