02864b1a12971f2a62b2932bd411a5c2b075b563
Darius <dariuszdroba@gmail.com>
2024-03-17 13:40:31 +0200

    Exception handling

---
+++ ExceptionHandling/InvalidUserNameException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class InvalidUserNameException : Exception
+    {
+        public InvalidUserNameException() { }
+        public InvalidUserNameException(string name) : base(String.Format("Invalid Student Name: {0}",name)) { 
+        }
+    }
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = null;
+            try
+            {
+                user = new User();
+                user.UserName = "Darius123";
+                ValidateUser(user);
+            }
+            catch(InvalidUserNameException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ValidateUser(User user)
+        {
+            Regex regex = new Regex("^[a-zA-Z]+$");
+            if(!regex.IsMatch(user.UserName))
+            {
+                throw new InvalidUserNameException(user.UserName);
+            }
+        }
+    }
+}
